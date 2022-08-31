a8f77b26a10777671bad20c1856ef5125e13622f
Federico Moyá <federicomoyamartin@gmail.com>
2022-08-31 19:30:12 -0300

    Redirect root to cc base rubocop gh file

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'welcome#index'
+  root to: redirect('https://raw.githubusercontent.com/codeclimate/styleguide/master/ruby/rubocop.yml')
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
